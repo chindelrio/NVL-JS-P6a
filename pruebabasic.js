cb11bba08056b3ff1b61fc8f3f11d774bb628beb
Antonio José <chindelrio@gmail.com>
2019-01-07 16:06:34 +0100

    Create pruebabasic.js

---
+++ pruebabasic.js
@@ -0,0 +1,10 @@
+var miNombre 
+miNombre = 'Antonio José Chinchetru'
+var miEdad 
+miedad= 46
+var diasSemana
+diasSemana = [1,'lunes',2,'martes',3,'miécoles',4,'jueves',5,'viernes',6,'sábado',7,'domingo']
+var valorGravedad
+valorGravedad = '9,8 s/m2'
+var numerosPrimos
+numerosPrimos = [1,3,5]
